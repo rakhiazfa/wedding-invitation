10bb59e4176333f99f8cd80a46b50181cb0fc507
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-27 04:44:58 +0700

    Update feature.

--- resources/views/guest/invitation_accepted.blade.php
+++ resources/views/guest/invitation_accepted.blade.php
@@ -6,10 +6,10 @@
 
             <div class="normal-wrapper min-h-screen flex justify-center items-center">
 
-                <div class="relative bg-white w-[400px] min-h-screen flex justify-center items-center p-5">
+                <div class="relative bg-white w-[400px] min-h-screen px-5 py-10">
 
                     <div>
-                        <h1 class="text-lg font-medium mb-5">Guest Details</h1>
+                        <h1 class="text-lg text-center font-medium mb-5">Guest Details</h1>
 
                         <table class="table table-sm table-bordered">
                             <tr>
