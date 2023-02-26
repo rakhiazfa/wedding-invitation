b1935a863d9eac37bece17c71609bdb8ae0d95f7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-27 04:44:08 +0700

    Update feature.

--- resources/views/guest/invitation_accepted.blade.php
+++ resources/views/guest/invitation_accepted.blade.php
@@ -6,18 +6,22 @@
 
             <div class="normal-wrapper min-h-screen flex justify-center items-center">
 
-                <div class="relative bg-white w-[400px] min-h-screen p-5">
+                <div class="relative bg-white w-[400px] min-h-screen flex justify-center items-center p-5">
 
-                    <table class="table table-sm table-bordered">
-                        <tr>
-                            <th>Name</th>
-                            <td>: {{ $invitation->guest_name }}</td>
-                        </tr>
-                        <tr>
-                            <th>Time Received</th>
-                            <td>: {{ $invitation->time_received }}</td>
-                        </tr>
-                    </table>
+                    <div>
+                        <h1 class="text-lg font-medium mb-5">Guest Details</h1>
+
+                        <table class="table table-sm table-bordered">
+                            <tr>
+                                <th>Name</th>
+                                <td>: {{ $invitation->guest_name }}</td>
+                            </tr>
+                            <tr>
+                                <th>Time Received</th>
+                                <td>: {{ $invitation->time_received }}</td>
+                            </tr>
+                        </table>
+                    </div>
 
                 </div>
 
