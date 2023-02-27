71a225634eb244c796f25fe45b720f3187401005
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-27 16:22:48 +0700

    Minify HTML.

---
+++ app/Http/Middleware/MinifyHtml.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MinifyHtml
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $regex = '#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))#';
+
+        $minified = preg_replace($regex, " ", $response->getContent());
+
+        $response->setContent($minified);
+
+        return $response;
+    }
+}
